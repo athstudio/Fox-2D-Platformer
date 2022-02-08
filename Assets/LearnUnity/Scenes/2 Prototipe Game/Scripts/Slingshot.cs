d3e1897a90db23b2b4ac6f9a2858d7ec09cdb7a7
Qvelent <48782273+Qvelent@users.noreply.github.com>
2022-02-08 23:27:36 +0300

    w5

---
+++ Assets/LearnUnity/Scenes/2 Prototipe Game/Scripts/Slingshot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        Debug.Log("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Slingshot:OnMouseEXit()");
+    }
+}
